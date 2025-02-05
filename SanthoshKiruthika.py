3faa79c95dc3d7899b643da9481abf50d02c3fbe
Santhoshkirukki <130481799+Santhoshkirukki@users.noreply.github.com>
2025-02-05 22:04:27 +0530

    Create SanthoshKiruthika.py

---
+++ SanthoshKiruthika.py
@@ -0,0 +1,9 @@
+# This Python script displays a message
+def main():
+    # Define the message
+    message = "santhoshkumar loves kiruthika"
+    # Print the message to the console
+    print(message)
+
+if __name__ == "__main__":
+    main()
